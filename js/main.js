907ef2eaa98d3f5701d7b41f85a9e0195f7fe967
imon-sheikh <mdimonhossain2017@gmail.com>
2024-09-26 18:30:47 +0600

    date get read and update for donatio by folloing CRUD operation

--- js/main.js
+++ js/main.js
@@ -5,14 +5,18 @@ donateNow.addEventListener('click', function(){
   const card1InputValue = getInputValueById('card1-input')
 //   console.log(card1InputValue);
 
-//initial +
+//initial ++
 const card1TextValue = getTextValueById('card1-balance')
-console.log(card1TextValue);
+// console.log(card1TextValue);
 
-//update +
+//update ++
 const updateCard1TextValue = card1InputValue + card1TextValue
  document.getElementById('card1-balance').innerText = updateCard1TextValue
   
  //initial --
- const totalBalance = getTextValueById('')
+ const totalBalance = getTextValueById('total-balance')
+
+//update --
+const updateTotalBalance = totalBalance - card1InputValue
+ document.getElementById('total-balance').innerText = updateTotalBalance
 })
